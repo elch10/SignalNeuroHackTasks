ead8f820e02e847978f64488ae042bc7bf578e1e
elch10 <elchin.kerimov.99@gmail.com>
2019-06-08 00:11:40 +0300

    Masked Layers

--- utils.py
+++ utils.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from matplotlib import colors
 
+import torch.nn as nn
+import torch.nn.functional as F
+
 def to_var(x, requires_grad=False):
     if torch.cuda.is_available():
         x = x.cuda()
@@ -33,8 +36,8 @@ def weight_prune_mask(model, pruning_perc):
 def diff_init_weight_prune_mask(model, pruning_perc):
     """
     Compute masks for model parameters so as pruning_perc smallest
-    changed parameters as compared to initial state of parameter
-    would be reset after multiplying masks by theirs parameters
+    changed parameters as compared to initial state of parameters
+    would be reset after multiplying masks by corresponding parameters
     """
     all_weights = []
     all_init_weights = []
@@ -182,3 +185,55 @@ def visualize(weights, title, figsize=None):
     plt.title(title)
     plt.axis('off')
     plt.tight_layout()
+
+
+class MaskedLinear(nn.Linear):
+    def __init__(self, in_features, out_features, bias=True):
+        super(MaskedLinear, self).__init__(in_features, out_features, bias)
+        self.mask_flag = False
+        self.mask = None
+
+    def set_mask(self, mask):
+        self.mask = mask
+        mask_var = self.get_mask()
+        self.weight.data = self.weight.data * mask_var.data
+        self.mask_flag = True
+
+    def get_mask(self):
+        return to_var(self.mask, requires_grad=False)
+
+    def forward(self, x):
+        if self.mask_flag:
+            mask_var = self.get_mask()
+            weight = self.weight * mask_var
+            return F.linear(x, weight, self.bias)
+        else:
+            return F.linear(x, self.weight, self.bias)
+
+class MaskedConv2d(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super(MaskedConv2d, self).__init__(in_channels, out_channels,
+                                           kernel_size, stride, padding,
+                                           dilation, groups, bias)
+        self.mask_flag = False
+        self.mask = None
+
+    def set_mask(self, mask):
+        self.mask = mask
+        mask_var = self.get_mask()
+        self.weight.data = self.weight.data * mask_var.data
+        self.mask_flag = True
+
+    def get_mask(self):
+        return to_var(self.mask, requires_grad=False)
+
+    def forward(self, x):
+        if self.mask_flag:
+            mask_var = self.get_mask()
+            weight = self.weight * mask_var
+            return F.conv2d(x, weight, self.bias, self.stride,
+                            self.padding, self.dilation, self.groups)
+        else:
+            return F.conv2d(x, self.weight, self.bias, self.stride,
+                            self.padding, self.dilation, self.groups)
